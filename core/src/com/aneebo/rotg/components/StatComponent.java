6afc465009fe420dfa6a1c2fe823cbf211d8c9e4
Kevin2test <Kevin@Kevin-PC.home>
2015-01-16 16:46:26 -0500

    Added to Code

---
+++ core/src/com/aneebo/rotg/components/StatComponent.java
@@ -0,0 +1,19 @@
+package com.aneebo.rotg.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Color;
+
+public class StatComponent extends Component {
+	public String name;
+	public float health;
+	public float energy;
+	public float eValue;
+	public Color color;
+	
+	public StatComponent(String name, float health, float energy, Color color) {
+		this.name = name;
+		this.health = health;
+		this.energy = energy;
+		this.color = color;
+	}
+}
