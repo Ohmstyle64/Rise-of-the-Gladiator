e0e7518690b108ed5d3847df609aba8908db72e0
Kevin2test <Kevin@Kevin-PC.home>
2015-01-19 23:16:51 -0500

    Missed in last merged. Sorry I'm tired!

---
+++ core/src/com/aneebo/rotg/systems/RegenSystem.java
@@ -0,0 +1,44 @@
+package com.aneebo.rotg.systems;
+
+import com.aneebo.rotg.components.StatComponent;
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.utils.ImmutableArray;
+
+public class RegenSystem extends EntitySystem {
+	
+	public ComponentMapper<StatComponent> sc = ComponentMapper.getFor(StatComponent.class);
+	public ImmutableArray<Entity> entities;
+	
+	public StatComponent stat;
+	public Entity e;
+	
+	@Override
+	public void addedToEngine(Engine engine) {
+		entities = engine.getEntitiesFor(Family.getFor(StatComponent.class));
+	}
+	
+	@Override
+	public void update(float deltaTime) {
+		
+		int size = entities.size();
+		for(int i = 0; i < size; i++) {
+			e = entities.get(i);
+			stat = sc.get(e);
+			
+			stat.health += stat.health_regen*(deltaTime / 6);
+			if(stat.health >= stat.max_health) stat.health = stat.max_health;
+			
+			stat.energy += stat.energy_regen*(deltaTime / 6);
+			if(stat.energy >= stat.max_energy) stat.energy = stat.max_energy;
+		}
+	}
+	
+	public void dispose() {
+		
+	}
+	
+}
